292c8eba8c5f28c141a9b088f52cd06871c4e920
Andreas Abraham <andreas.abraham@egiz.gv.at>
2018-09-06 16:42:02 +0200

    constants added

---
+++ utils/src/main/java/at/gv/egiz/upater/Constants.java
@@ -0,0 +1,10 @@
+package at.gv.egiz.upater;
+
+public class Constants {
+	
+	public static final String WEBSTART_URL = "https://webstart.buergerkarte.at/";
+	public static final String MOCCA_STR    = "mocca/";
+	public static final String VERSION_FILE = "Release.txt";
+	public static final String PATH_TO_VERSION_FILE = WEBSTART_URL + MOCCA_STR + VERSION_FILE; 
+
+}
