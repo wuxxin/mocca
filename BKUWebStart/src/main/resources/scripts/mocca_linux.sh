58be7a313c38cdb37fe82e343ef2fc84c72c71b7
Andreas Abraham <andreas.abraham@egiz.gv.at>
2018-09-07 14:44:56 +0200

    startup scripts for mocca added

---
+++ BKUWebStart/src/main/resources/scripts/mocca_linux.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+#This is a start script for mocca on linux
+java -Djavax.net.ssl.trustStoreType=jks -Djavax.net.ssl.keyStoreType=jks -jar "BKUWebStart-1.3.35-SNAPSHOT.jar"
---
+++ BKUWebStart/src/main/resources/scripts/mocca_win.bat
@@ -0,0 +1,4 @@
+REM This is a start script for mocca on windows 
+echo off 
+start /b java -Djavax.net.ssl.trustStoreType=jks -Djavax.net.ssl.keyStoreType=jks -jar BKUWebstart-1.3.35-SNAPSHOT.jar
+
